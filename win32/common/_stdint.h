4576fc4f87cec6532487cf19f5492a7f33331337
Konstantinos Sofokleous <ksophocleous@gmail.com>
2014-10-28 09:40:55 +0100

    initial commit from tar 1.4.3 in http://gstreamer.freedesktop.org/src/gst-plugins-base

---
+++ win32/common/_stdint.h
@@ -0,0 +1,9 @@
+#ifndef _GST_PLUGINS_BASE__STDINT_H
+#define _GST_PLUGINS_BASE__STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "gst-plugins-base 1.4.3"
+/* generated using gnu compiler gcc (Debian 4.9.1-15) 4.9.1 */
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
